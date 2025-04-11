270f2d3e487d0a14d819c164ba233ad805d3bdcd
natnaelengeda <nattynengeda@gmail.com>
2025-04-11 20:38:54 +0300

    Primary Commit

---
+++ src/routes/about.tsx
@@ -0,0 +1,11 @@
+import { createFileRoute } from '@tanstack/react-router'
+
+export const Route = createFileRoute('/about')({
+  component: About,
+})
+
+export default function About() {
+  return (
+    <div>About Page</div>
+  )
+}
---
+++ src/routes/index.tsx
@@ -0,0 +1,11 @@
+import { createFileRoute } from '@tanstack/react-router'
+
+export const Route = createFileRoute('/')({
+  component: Index,
+})
+
+export default function Index() {
+  return (
+    <div>Index</div>
+  )
+}

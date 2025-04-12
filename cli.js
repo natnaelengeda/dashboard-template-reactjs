4273347a8d26237c0a5d2d959f253ffbcbe9fef6
natnaelengeda <nattynengeda@gmail.com>
2025-04-12 16:38:09 +0300

    feat: update package configuration and add CLI for template download

---
+++ cli.js
@@ -0,0 +1,34 @@
+#!/usr/bin/env node
+
+const path = require("path")
+const fs = require("fs")
+const execa = require("execa")
+const download = require("download-git-repo")
+
+const templateRepo =
+  "https://github.com/natnaelengeda/dashboard-template-reactjs.git" // Your repo name
+const targetDir = process.argv[2] || "." // Directory where to place the files
+
+if (!fs.existsSync(targetDir)) {
+  fs.mkdirSync(targetDir, { recursive: true });
+}
+
+console.log(`Downloading template to ${targetDir}...`);
+
+
+download(templateRepo, targetDir, { clone: true }, (err) => {
+  if (err) {
+    console.error('Error downloading template:', err);
+    process.exit(1);
+  }
+
+  console.log('Template downloaded!');
+  console.log('Now running npm install...');
+  execa('npm', ['install'], { cwd: targetDir })
+    .then(() => {
+      console.log('npm install complete!');
+    })
+    .catch((err) => {
+      console.error('Error running npm install:', err);
+    });
+});
